1aa24cf4e25496ee98c3a971adb316be1fa05ad1
Teju <Teju@DESKTOP-Q9IM2N1.mshome.net>
2024-09-15 09:32:06 +0530

    NewCommit

--- PlacementM_Certificate/src/main/java/com/tnsif/pm/certificate/CertificateController.java
+++ PlacementM_Certificate/src/main/java/com/tnsif/pm/certificate/CertificateController.java
@@ -1,4 +1,3 @@
-
 package com.tnsif.pm.certificate;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,105 +7,71 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CertificateController 
-{
-	@Autowired
-	private CertificateService Service;
-	
-	//Restful API methods for retrieval operations
-	
-	@GetMapping("/certificates")
-	public List<Certificate> list()
-	{
-		return Service.listAll();
-	}
-	
-	@GetMapping("/certificates/{id}")
+@RequestMapping("/api/stores")
+@CrossOrigin(origins = "http://localhost:3000")  // Allow requests only from this origin
+public class CertificateController {
 
-	public ResponseEntity<Certificate> get (@PathVariable Long id)
-
-	{
-
-	try
-
-	{
-
-	Certificate certificate = Service.get(id);
-
-	return new ResponseEntity<Certificate>(certificate, HttpStatus.OK);
-
-	}
-
-	catch (NoSuchElementException e)
-
-	{
-
-	return new ResponseEntity<Certificate>(HttpStatus.NOT_FOUND);
-
-	}
-
-	}
-	
-	//create operation
-	@PostMapping("/certificates")
-
-	public void add (@RequestBody Certificate certificate)
-
-	{
-
-	Service.save(certificate);
-
-	}
-	
-	
-	// RESTful API method for Update operation
-
-	@PutMapping("/products/{id}")
-
-	public ResponseEntity<?> update (@RequestBody Certificate certificate, @PathVariable Long id)
-
-	{
-
-	try
-
-		{
-	
-			Certificate existCertificate = Service.get(id);
-		
-			Service.save(existCertificate);
-		
-			return new ResponseEntity<>(HttpStatus.OK);
-	
-			}
-
-	catch (NoSuchElementException e)
-	
-		{
-	
-	
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	// Restful API method for Delete operation
-
-	@DeleteMapping("/certificate/{id}")
-
-	public void delete (@PathVariable Long id)
-
-	{
-
-	Service.delete(id);
-
-	}
-
+    @Autowired
+    private CertificateService service;
+    
+    // Restful API method for retrieval of all certificates
+    @GetMapping("/certificates")
+    public List<Certificate> list() {
+        return service.listAll();
+    }
+    
+    // Restful API method for retrieving a certificate by ID
+    @GetMapping("/certificates/{id}")
+    public ResponseEntity<Certificate> get(@PathVariable Long id) {
+        try {
+            Certificate certificate = service.get(id);
+            return new ResponseEntity<>(certificate, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // Restful API method for creating a new certificate
+    @PostMapping("/certificates")
+    public void add(@RequestBody Certificate certificate) {
+        service.save(certificate);
+    }
+    
+    // Restful API method for updating an existing certificate by ID
+    @PutMapping("/certificates/{id}")
+    public ResponseEntity<?> update(@RequestBody Certificate certificate, @PathVariable Long id) {
+        try {
+            Certificate existCertificate = service.get(id);
+            if (existCertificate != null) {
+                // Update the existing certificate with new data
+                existCertificate.setCertificateName(certificate.getCertificateName());
+                existCertificate.setIssuingOrganization(certificate.getIssuingOrganization());
+                existCertificate.setIssueDate(certificate.getIssueDate());
+                existCertificate.setStudentId(certificate.getStudentId());
+                
+                service.save(existCertificate);  // Save updated certificate
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // Restful API method for deleting a certificate by ID
+    @DeleteMapping("/certificates/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
